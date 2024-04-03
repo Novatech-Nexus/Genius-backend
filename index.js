2b55b0bed04e79ed61fcd7cfcadc51dbb6313f12
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-04-03 11:48:41 +0530

    removed index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import  express  from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-dotenv.config();
+// import  express  from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// dotenv.config();
 
-mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-    console.log('connected to mongoDB');
-    }).catch((err) => {
-        console.log(err);
-    })
+// mongoose
+//     .connect(process.env.MONGO)
+//     .then(() => {
+//     console.log('connected to mongoDB');
+//     }).catch((err) => {
+//         console.log(err);
+//     })
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-app.listen(5050, () =>{
-    console.log('Server is running on port 5050!');
-    }
-);
+// app.listen(5050, () =>{
+//     console.log('Server is running on port 5050!');
+//     }
+// );
