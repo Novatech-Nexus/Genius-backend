29ae6e3b4e093281b99526eb8c32781a9a0be597
Buddhima <128503846+Zeus4808@users.noreply.github.com>
2024-04-11 16:54:27 +0530

    add env file and connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,18 @@
-import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from 'mongoose';
 
-async function connect(){
-    const mongod =await MongoMemoryServer.create();
-    const getUri = mongod.getUri();
-
-    mongoose.set('strictQuery', true)
-    const db = await mongoose.connect(getUri);
-    console.log("Database connected");
-    return db;
+async function connect() {
+    try {
+        const uri = "mongodb+srv://ITPM2024:ITPM2024@atlascluster.rhs2ett.mongodb.net/?retryWrites=true&w=majority";
+        const db = await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connected");
+        return db;
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
 }
 
 export default connect;
