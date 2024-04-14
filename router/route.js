2c800e0ee39fa1f588131c144e0478762e0a6897
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-04-14 14:44:35 +0530

    made some changes in route in verifyOTP route.

--- router/route.js
+++ router/route.js
@@ -8,14 +8,14 @@ import Auth, {localVariables} from '../middleware/auth.js';
 // POST methods
 router.route('/register').post(controller.register); // register user
 router.route('/registerMail').post(); // send the email
-router.route('/authenticate').post((req,res) => res.end()); // authenticate the user
+router.route('/authenticate').post(controller.verifyUser, (req,res) => res.end()); // authenticate the user
 router.route('/login').post(controller.verifyUser, controller.login); // login in app
 
 
 // GET Methods
 router.route('/user/:email').get(controller.getUser); // user with email
 router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP); //generate random OTP
-router.route('/verifyOTP').get(controller.verifyOTP); //verify generated OTP
+router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP); //verify generated OTP
 router.route('/createResetSession').get(controller.createResetSession); // reset all the variables
 
 
