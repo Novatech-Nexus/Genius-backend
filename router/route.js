a187bfa7fc5113f610eecb359ba24903d5824237
Lakshitha <IT22172082@my.sliit.lk>
2024-04-15 20:39:12 +0530

    Solved the issue happened when commiting.

--- router/route.js
+++ router/route.js
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import Employees from '../model/employee.js';
 
 const router = Router();
 
@@ -21,5 +22,41 @@ router.route('/createResetSession').get(controller.createResetSession); // reset
 // PUT Methods
 router.route('/updateUser').put(controller.updateUser); // is use to update the user profile
 router.route('/resetPassword').put(controller.resetPassword); // use to reset password
+
+
+//Staff management routes
+//test
+router.get("/test", (req, res) => res.send("Employee routes working"));
+
+router.post("/", (req, res) => {
+  Employees.create(req.body)
+    .then(() => res.json({ msg: "Employee added successfully" }))
+    .catch(() => res.status(400).json({ msg: "Employee adding failed" }));
+});
+
+router.get("/", (req, res) => {
+  Employees.find()
+    .then((employees) => res.json(employees))
+    .catch(() => res.status(400).json({ msg: "No employees found" }));
+});
+
+router.get("/:id", (req, res) => {
+  Employees.findById(req.params.id)
+    .then((employee) => res.json(employee))
+    .catch(() => res.status(400).json({ msg: "Cannot find this employee" }));
+});
+
+router.put("/:id", (req, res) => {
+  Employees.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.json({ msg: "Update Successfully" }))
+    .catch(() => res.status(400).json({ msg: "Update failed" }));
+});
+
+router.delete("/:id", (req, res) => {
+  Employees.findByIdAndDelete(req.params.id)
+    .then(() => res.json({ msg: "Delete successfully" }))
+    .catch(() => res.status(400).json({ msg: "Cannot delete" }));
+});
+
 
 export default router;
